dd0eec52f0a8118e1062cb0ea6cf41e3cd4acfa3
WaysideTester <dylanmurray211@outlook.com>
2019-03-27 18:07:18 -0500

    intro to controllers

---
+++ SwordandFather/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwordandFather.Models;
+
+namespace SwordandFather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        static List<User> _users = new List<User>();
+
+        [HttpPost("register")]
+        public ActionResult<int> AddUser(User newUser)
+        {
+            newUser.Id = _users.Count + 1;
+
+            _users.Add(newUser);
+
+            return Created($"api/users/{newUser.Id}", newUser);
+        }
+    }
+}
---
+++ SwordandFather/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwordandFather.Models
+{
+    public class User
+    {
+        public string Username { get; }
+        public string Password { get; }
+        public int Id { get; set; }
+    }
+}
